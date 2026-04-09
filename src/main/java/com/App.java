d956b266a826c66540d1209807aeaf188a48c4e0
yassine <elasriyassine000@gmail.com>
2026-04-09 19:56:47 +0100

    initial commit

---
+++ src/main/java/com/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("CI/CD with Jenkins 🚀");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
